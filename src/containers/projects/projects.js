c0103a46c24e5b609fc7ccb7f6fe4805eb7f3981
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-10-16 00:39:19 +0200

    add logic for the projects carousel

--- src/containers/projects/projects.js
+++ src/containers/projects/projects.js
@@ -1,31 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './projects.module.css';
 import arrow from '../../assets/arrow.png';
 import carouselArrow from '../../assets/carousel-arrow.png';
+import { myProjects } from './projectsIndex';
 
-const projects = props => {
-  let projectName = 'Newsweek';
-  let githubLink = 'Newsweek';
-  let pageLink = 'Newsweek';
+const Projects = props => {
+  const [ previousProject, setPreviousProject ] = useState(myProjects[0]);
+  const [ currentProject, setCurrentProject ] = useState(myProjects[1]);
+  const [ nextProject, setNextProject ] = useState(myProjects[2]);
+
+  let projectName = currentProject.name;
+  let githubLink = currentProject.github;
+  let pageLink = currentProject.link;
+
+  const onNextHandler = () => {
+    setPreviousProject(currentProject);
+    setCurrentProject(nextProject);
+    for (let i = 0; i < myProjects.length; i++) {
+      if (nextProject === myProjects[i] && i !== myProjects.length - 1) {
+        setNextProject(myProjects[i + 1]);
+      } else if (nextProject === myProjects[i] && i === myProjects.length - 1) {
+        setNextProject(myProjects[0]);
+      }
+    }
+  }
+
+  const onPreviousHandler = () => {
+    setNextProject(currentProject);
+    setCurrentProject(previousProject);
+    for (let i = 0; i < myProjects.length; i++) {
+      if (
+        previousProject === myProjects[i] &&
+        i !== 0
+      ) {
+        setPreviousProject(myProjects[i - 1]);
+      } else if (
+        previousProject === myProjects[i] &&
+        i === 0
+      ) {
+        setPreviousProject(myProjects[myProjects.length - 1]);
+      }
+    }
+  }
 
   return (
     <div className={styles.ProjectsComponent}>
       <h2 className={styles.Projects}>Projects</h2>
       <div className={styles.CarouselContainer}>
-        <div className={styles.Prev}></div>
-        <div className={styles.Current}></div>
-        <div className={styles.Next}></div>
+        <div className={styles.ProjGifs}>
+          <img src={previousProject.gif} className={styles.PrevProj} alt='' />
+          <img src={currentProject.gif} className={styles.CurrentProj} alt='' />
+          <img src={nextProject.gif} className={styles.NextProj} alt='' />
+        </div>
         <div className={styles.NameContainer}>
-          <img className={styles.PrevArrow} src={carouselArrow} alt='' />
+          <img
+            onClick={onPreviousHandler}
+            className={styles.PrevArrow}
+            src={carouselArrow}
+            alt=''
+          />
           <p className={styles.ProjectName}>{projectName}</p>
-          <img className={styles.NextArrow} src={carouselArrow} alt='' />
+          <img
+            onClick={onNextHandler}
+            className={styles.NextArrow}
+            src={carouselArrow}
+            alt=''
+          />
         </div>
         <div className={styles.LinksContainer}>
-          <a className={styles.Link} href={githubLink}>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.Link}
+            href={githubLink}
+          >
             See Github
           </a>
-          <a className={styles.Link} href={pageLink}>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.Link}
+            href={pageLink}
+          >
             See Project
           </a>
         </div>
@@ -35,4 +92,4 @@ const projects = props => {
   );
 };
 
-export default projects;
+export default Projects;
---
+++ src/containers/projects/projectsIndex.js
@@ -0,0 +1,93 @@
+export const myProjects = [
+  {
+    name: 'The Next Web Clone',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065139/project-gifs/tnw-clone_vssbhd.gif',
+    github: 'https://github.com/kikupiku/tnw-clone',
+    link: 'https://kikupiku.github.io/tnw-clone/',
+  },
+  {
+    name: 'Battleships',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065128/project-gifs/battleships_udxrye.gif',
+    github: 'https://github.com/kikupiku/battleships',
+    link: 'https://kikupiku.github.io/battleships/',
+  },
+  {
+    name: 'Weather App',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065139/project-gifs/weather-app_dg3bmn.gif',
+    github: 'https://github.com/kikupiku/weather-app',
+    link: 'https://kikupiku.github.io/weather-app/',
+  },
+  {
+    name: 'Game Inventory',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065130/project-gifs/game-inventory_fcitno.gif',
+    github: 'https://github.com/kikupiku/game-inventory',
+    link: 'https://game-inventory-kikupiku.herokuapp.com/',
+  },
+  {
+    name: 'Odinbook',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065129/project-gifs/odinbook_coepbo.gif',
+    github: 'https://github.com/kikupiku/fakebook',
+    link: 'https://odinbook-kikupiku.herokuapp.com/',
+  },
+  {
+    name: 'Pomodoro Timer',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065128/project-gifs/pomodoro-timer_ohl1qj.gif',
+    github: 'https://github.com/kikupiku/pomodoro-clock',
+    link: 'https://kikupiku.github.io/pomodoro-clock/',
+  },
+  {
+    name: 'Etch-a-Sketch',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065127/project-gifs/etch-a-sketch_rgvwqo.gif',
+    github: 'https://github.com/kikupiku/Etch-a-Sketch',
+    link: 'https://kikupiku.github.io/Etch-a-Sketch/',
+  },
+  {
+    name: 'Newsweek Clone',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065137/project-gifs/newsweek-clone_revdwd.gif',
+    github: 'https://github.com/kikupiku/newsweek-clone',
+    link: 'https://kikupiku.github.io/newsweek-clone/',
+  },
+  {
+    name: 'To Do List',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065130/project-gifs/to-do-list_msitq0.gif',
+    github: 'https://github.com/kikupiku/to-do-list',
+    link: 'https://kikupiku.github.io/to-do-list/',
+  },
+  {
+    name: 'Whisper Campaign',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597065130/project-gifs/whisper-campaign_idgvdk.gif',
+    github: 'https://github.com/kikupiku/members-club',
+    link: 'https://whisper-campaign.herokuapp.com/',
+  },
+  {
+    name: 'Apple Page Clone',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597070449/project-gifs/apple-clone_gqqac5.gif',
+    github: 'https://github.com/kikupiku/apple-page-copy',
+    link: 'https://kikupiku.github.io/apple-page-copy/',
+  },
+  {
+    name: 'Talkative Calculator',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597275130/project-gifs/calculator_agas08.gif',
+    github: 'https://github.com/kikupiku/Calculator',
+    link: 'https://kikupiku.github.io/Calculator/',
+  },
+  {
+    name: 'Online Library',
+    gif:
+      'https://res.cloudinary.com/kikupiku/image/upload/v1597355809/project-gifs/online-library_hjopso.gif',
+    github: 'https://github.com/kikupiku/book-library',
+    link: 'https://kikupiku.github.io/book-library/',
+  },
+];
